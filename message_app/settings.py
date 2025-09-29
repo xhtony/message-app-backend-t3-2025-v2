d207f02b1aa54bdd5698dbae4ad41b760692b498
xiaohuichi <xiaohui.chi.nz@gmail.com>
2025-09-29 14:28:59 +1300

    1. intalll .env 2.created .gitignore 3.setup database

--- message_app/settings.py
+++ message_app/settings.py
@@ -9,8 +9,11 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,11 +83,11 @@ WSGI_APPLICATION = "message_app.wsgi.app"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "neondb",
-        "USER": "neondb_owner",
-        "PASSWORD": "npg_LF2juDR8eZCG",
-        "HOST": "ep-billowing-bonus-ad8xw8am-pooler.c-2.us-east-1.aws.neon.tech",
-        "PORT": "5432",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
     }
 }
 
