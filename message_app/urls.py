ae848214baee3db4cd623c99424909965914e96a
xiaohuichi <xiaohui.chi.nz@gmail.com>
2025-10-13 13:55:19 +1300

    1. create dockerfile 2. create docker-compose.yml

--- message_app/urls.py
+++ message_app/urls.py
@@ -18,7 +18,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+path("admin/", admin.site.urls),
 urlpatterns = [
-    path("admin/", admin.site.urls),
     path("api/", include("message.urls"))
 ]
