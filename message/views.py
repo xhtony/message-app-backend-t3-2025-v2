5a831455d06afd02d29a33cecd6890ba6137ab8c
xiaohuichi <xiaohui.chi.nz@gmail.com>
2025-10-13 14:39:52 +1300

    1. create send message 1. create dockerfile

--- message/views.py
+++ message/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from message.models import Message
+
 
 # Create your views here.
+@api_view(['POST'])
+def send_message(request):
+    from django.contrib.messages.storage.cookie import MessageSerializer
+    message_serializer = MessageSerializer(data=request.data)
+    if message_serializer.is_valid():
+        message_serializer.save()
+        return Response({'message': 'Message sent successfully'},
+                        status=status.HTTP_201_CREATED)
